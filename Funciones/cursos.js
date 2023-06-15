a21d9fb89512e7820d6478ef938ff86f41274cf7
Cori <131205161+corizapata@users.noreply.github.com>
2023-06-15 17:52:28 -0300

    Add files via upload

---
+++ Funciones/cursos.js
@@ -0,0 +1,16 @@
+const cursos = [
+
+    {
+        id: 1,
+        nombre: "Programación Web",
+        descripcion: "",
+        capacitador:"",
+        duración: "3 meses",
+        categoria: {
+            nombre: "Programacion, diseño, laboratorio, audiovisual, electronica",
+            img: "/logo/programacion.png"
+        }
+
+    }
+
+]
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mi primer sitio web Conectar LAB
+<a target="_blank" href="">Este es mi primer sitio web :))</a>
